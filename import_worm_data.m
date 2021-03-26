51acea73fa5cca0a3efc9cc9a6163bd5acbd7d18
PaulMCarrillo <72674231+PaulMCarrillo@users.noreply.github.com>
2021-03-26 17:13:31 -0500

    Update import_worm_data.m

--- import_worm_data.m
+++ import_worm_data.m
@@ -1,12 +1,11 @@
 %Function to take 101x101 pixel worm pictures and convert them to
 %usable data.
-
-function [worm_data] = import_worm_data()
+function [worm_data] = import_worm_data(num_of_pics)
 
 %Create image value matrix
-worm_data = zeros(10201, 5480);
+worm_data = zeros(10201, num_of_pics);
 
-for i = 1:5480
+for i = 1:num_of_pics
     
     %Import file names iterativley and store image pixel values in cval 
     %This file path is exclusive to my computer!
