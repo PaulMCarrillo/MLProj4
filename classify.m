0c0e1dd2a44436a05427022375af4ea1b44c6bb7
Gautham Viswanadha Varma Mudunuru <32221326+mgauthamvarma@users.noreply.github.com>
2021-04-19 17:12:38 -0500

    Update classify.m

--- classify.m
+++ classify.m
@@ -2,14 +2,14 @@
 %test data
 
 function [y] = classify(y_hat)
-
+m = mean(y_hat);
 y = zeros(1, size(y_hat, 2));
 
     for i = 1:size(y_hat, 2)
-        if y_hat(1, i) >= 0.5
+        if y_hat(1, i) >= m
             y(1, i) = 1;
         end
-        if y_hat(1, i) < 0.5
+        if y_hat(1, i) < m
             y(1, i) = 0;
         end
     end
